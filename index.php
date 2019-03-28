83872934cc95c41a2379f618a45fe06a0cb9ff1e
Vic <vcao@findsomewinmore.com>
2019-03-28 15:46:33 -0400

    Skip status screen if empty

--- index.php
+++ index.php
@@ -438,7 +438,9 @@ if($ip == '67.9.125.106' || $ip == '172.23.0.1'):
                 } elseif($noslides % 4 == 0) {
 
                     $json = file_get_contents('https://slack.com/api/users.list?token=xoxp-3921626273-114425188213-401339713923-9e939340d027352b450c1e6fdf421ce6&presence=true&pretty=true');
-                    $obj = json_decode($json);
+                    $obj = json_decode($json, true);
+
+                    if (!empty($obj)):
                     ?>
                     <div class="slide schedule schedule client board" data-attr="20000" id="flight-status" style="background: none;">
 
@@ -483,7 +485,7 @@ if($ip == '67.9.125.106' || $ip == '172.23.0.1'):
                         </div>
 
                     </div>
-                <?php
+                <?php endif;
                 } elseif($noslides % 6 == 0){ ?>
                     <?php
                     if( have_rows('client_schedule', 'options') ): ?>
